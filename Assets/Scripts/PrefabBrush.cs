f2f3ada03d137796a5ce5cda4dd8333df14d6026
Oluwapelumi Ayeni <noluwapelumi4@gmail.com>
2022-12-02 13:02:19 +0000

    Added New Controller

---
+++ Assets/Scripts/PrefabBrush.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Tilemaps;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Prefab Brush", menuName = " Brushes/Prefab Brush")]
+[CustomGridBrush(false, true, false, "Prefab Brush")]
+public class PrefabBrush : GameObjectBrush
+{
+    public override void Erase(GridLayout gridLayout, GameObject brushTarget, Vector3Int position)
+    {
+        if (brushTarget.layer == 31) return;
+        Transform erased = GetObjectInCell(gridLayout, brushTarget.transform, new Vector3Int(position.x, position.y,0));
+        if (erased != null) Undo.DestroyObjectImmediate(erased.gameObject);
+    }
+
+    private static Transform GetObjectInCell(GridLayout gridLayout, Transform parent, Vector3Int position)
+    {
+        int childCount = parent.childCount;
+        Vector3 min = gridLayout.LocalToWorld(gridLayout.CellToLocalInterpolated(position));
+        Vector3 max = gridLayout.LocalToWorld(gridLayout.CellToLocalInterpolated(position + Vector3Int.one));
+        Bounds bounds = new Bounds((max + min) * 0.5f, max - min);
+
+        for (int i = 0; i < childCount; i++)
+        {
+            Transform child = parent.GetChild(i);
+            if (bounds.Contains(child.position))  return child;
+        }
+        return null;
+    }
+}
